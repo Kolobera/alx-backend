69e7a34f649a7c51e3d5cec7ec026e38b4db5892
Ousmane Konate <konatealim@gmail.com>
2023-04-25 23:36:06 +0200

    add task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,9 +22,6 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> str:
     '''Get locale from request.'''
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,13 +22,16 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> str:
     '''Get locale from request.'''
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     '''Basic Flask app.'''
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
